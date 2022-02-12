8ce4f3b707c62c68cb64a04d6e0b62d78f57836f
이성준 <thwn40@naver.com>
2022-02-13 00:04:55 +0900

    20220213

--- src/InfreanAlgorithm/infrean0605.java
+++ src/InfreanAlgorithm/infrean0605.java
@@ -1,35 +1,26 @@
 package InfreanAlgorithm;
 
-<<<<<<< HEAD
 
-
-=======
->>>>>>> cba115a2bbe1a0ec517763da2fcb1fc305d5060f
 import java.util.*;
 
 
 
 public  class infrean0605 {
     static Scanner kb = new Scanner(System.in);
+
     public static void solution(int n) {
         int[] arr1 = new int[n];
         Set<Integer> arr = new HashSet<Integer>();
         for (int i = 0; i < n; i++) {
-            int temp =kb.nextInt();
+            int temp = kb.nextInt();
             arr.add(temp);
-            arr1[i]=temp;
+            arr1[i] = temp;
         }
 
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> cba115a2bbe1a0ec517763da2fcb1fc305d5060f
-        if(arr.size()== arr1.length){
+        if (arr.size() == arr1.length) {
             System.out.println("U");
-        }
-        else{
+        } else {
             System.out.println("D");
         }
 
@@ -37,25 +28,13 @@ public  class infrean0605 {
     }
 
 
-
-
     public static void main(String[] args) {
         int a = kb.nextInt();
-
 
 
         solution(a);
 
 
-
-
-
     }
 
-<<<<<<< HEAD
 }
-
-
-=======
-}
->>>>>>> cba115a2bbe1a0ec517763da2fcb1fc305d5060f
